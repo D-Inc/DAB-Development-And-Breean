dc7b4cd74975d91f0094af10e7552417050fb349
ninjawarrior1337 <t.rothled@gmail.com>
2016-06-27 14:06:39 -0700

    Changed mod id to "dab" and changed modname to DAB (Development And Breean)

--- src/main/java/com/rocker1337/dab/Reference.java
+++ src/main/java/com/rocker1337/dab/Reference.java
@@ -5,7 +5,7 @@ package com.rocker1337.dab;
  */
 public class Reference
 {
-    public static final String MODID = "DAB";
-    public static final String MODNAME = "DAB";
+    public static final String MODID = "dab";
+    public static final String MODNAME = "DAB (Development and Breean)";
     public static final String VERSION = "0.1";
 }
