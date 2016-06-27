cc6ceafd96507af6e313bf097de4f3ee94d291e8
ninjawarrior1337 <t.rothled@gmail.com>
2016-06-27 14:26:36 -0700

    Added DAB items file

---
+++ src/main/java/com/rocker1337/dab/init/DABItems.java
@@ -0,0 +1,24 @@
+package com.rocker1337.dab.init;
+
+import net.minecraft.item.Item;
+
+/**
+ * Created by troth on 6/27/2016.
+ */
+public class DABItems
+{
+    public static void init()
+    {
+
+    }
+
+    public static void registerRenders()
+    {
+
+    }
+
+    private static void  registerRender(Item item)
+    {
+
+    }
+}
