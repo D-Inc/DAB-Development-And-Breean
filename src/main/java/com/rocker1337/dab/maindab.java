6470a6d97a3250685aec59fd06d9ca8fdd18cd43
ninjawarrior1337 <ninjawarrior1337@users.noreply.github.com>
2016-06-26 20:28:36 -0700

    Create maindab.java

---
+++ src/main/java/com/rocker1337/dab/maindab.java
@@ -0,0 +1,42 @@
+package com.ninjawarrior1337.abos;
+
+
+import net.minecraft.client.main.Main;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+
+import static com.rocker1337.dab.Reference.MODID;
+
+@Mod(modid = MODID, name = Reference.MODNAME, version = Reference.VERSION)
+public class maindab
+{
+
+    @SidedProxy(clientSide="com.rocker1337.dab.ClientProxy", serverSide="com.rocker1337.dab.ServerProxy")
+    public static CommonProxy proxy;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent e)
+    {
+        System.out.println("Starting Pre Init");
+        this.proxy.preInit(e);
+    }
+    @EventHandler
+    public void init(FMLInitializationEvent e)
+    {
+        System.out.println("Starting Init");
+        this.proxy.init(e);
+    }
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent e)
+    {
+        System.out.println("Starting Post Init");
+        System.out.println("9+10=21");
+        this.proxy.postInit(e);
+    }
+}
