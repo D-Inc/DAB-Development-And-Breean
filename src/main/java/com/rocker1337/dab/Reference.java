4b7fe1cc6b70e700b9a01c044e30aa1e938aa8ba
ninjawarrior1337 <t.rothled@gmail.com>
2016-06-27 23:09:46 -0700

    Changed things

--- src/main/java/com/rocker1337/dab/Reference.java
+++ src/main/java/com/rocker1337/dab/Reference.java
@@ -4,7 +4,7 @@ public class Reference
 {
     public static final String MODID = "dab";
     public static final String MODNAME = "DAB (Development and Breean)";
-    public static final String VERSION = "0.11";
+    public static final String VERSION = "0.1";
     
     //Items
     public static enum DABItems
