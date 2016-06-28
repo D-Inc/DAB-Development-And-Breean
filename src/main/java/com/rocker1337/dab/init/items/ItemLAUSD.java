5f7b745805763fce8b3a4804efda92c66a69ed4d
ninjawarrior1337 <t.rothled@gmail.com>
2016-06-28 16:08:34 -0700

    Added LAUSD Item

--- src/main/java/com/rocker1337/dab/init/items/ItemLAUSD.java
+++ src/main/java/com/rocker1337/dab/init/items/ItemLAUSD.java
@@ -1,8 +1,13 @@
 package com.rocker1337.dab.init.items;
 
+import com.rocker1337.dab.Reference;
 import net.minecraft.item.Item;
 
 public class ItemLAUSD extends Item
 {
-
+    public ItemLAUSD()
+    {
+        setUnlocalizedName(Reference.DABItems.LAUSD.getUnlocalizedName());
+        setRegistryName(Reference.DABItems.LAUSD.getRegistryName());
+    }
 }
