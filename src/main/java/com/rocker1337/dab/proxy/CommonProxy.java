c2f023a6416d8695c1b7fe6052e4beb68aa6b342
ninjawarrior1337 <t.rothled@gmail.com>
2016-06-29 15:04:49 -0700

    Added new worldgen

--- src/main/java/com/rocker1337/dab/proxy/CommonProxy.java
+++ src/main/java/com/rocker1337/dab/proxy/CommonProxy.java
@@ -2,9 +2,11 @@ package com.rocker1337.dab.proxy;
 
 import com.rocker1337.dab.init.DABItems;
 import com.rocker1337.dab.init.crafting.DABCrafting;
+import com.rocker1337.dab.world.DabWorldGen;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class CommonProxy
 {
@@ -17,6 +19,7 @@ public class CommonProxy
     {
         DABItems.setCreativeTab();
         DABCrafting.initCrafting();
+        GameRegistry.registerWorldGenerator(new DabWorldGen(), 0);
 
     }
     public void postInit(FMLPostInitializationEvent e)
