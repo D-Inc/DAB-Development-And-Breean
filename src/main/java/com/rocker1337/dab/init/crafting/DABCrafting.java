ddae7348d008d1f7b976702be14911b88eae463a
ninjawarrior1337 <ninjawarrior1337@users.noreply.github.com>
2016-06-30 22:58:44 -0700

    Added leather recipe

--- src/main/java/com/rocker1337/dab/init/crafting/DABCrafting.java
+++ src/main/java/com/rocker1337/dab/init/crafting/DABCrafting.java
@@ -13,6 +13,6 @@ public class DABCrafting
         GameRegistry.addSmelting(Items.WHEAT_SEEDS, new ItemStack(DABItems.weed, 1, 0), 1000.0F);
         GameRegistry.addRecipe(new ItemStack(Blocks.LADDER, 4, 0), new Object[]{"W W","WWW","W W", 'W', new ItemStack(DABItems.weed, 1, 0)});
         GameRegistry.addShapelessRecipe(new ItemStack(Blocks.TNT, 64, 0), new Object[]{DABItems.pussy, DABItems.Derek});
-
+        GameRegistry.addSmelting(Items.LEATHER, new ItemStack(Items.ROTTEN_FLESH, 1, 0), 1000.0F);
     }
 }
