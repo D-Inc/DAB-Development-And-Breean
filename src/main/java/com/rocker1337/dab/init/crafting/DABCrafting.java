06daef2bf8e0f57d2be2811e9a88bd18cbcea44e
ninjawarrior1337 <t.rothled@gmail.com>
2016-06-30 23:24:03 -0700

    Added rotten flesh to leather

--- src/main/java/com/rocker1337/dab/init/crafting/DABCrafting.java
+++ src/main/java/com/rocker1337/dab/init/crafting/DABCrafting.java
@@ -13,7 +13,7 @@ public class DABCrafting
         GameRegistry.addSmelting(Items.WHEAT_SEEDS, new ItemStack(DABItems.weed, 1, 0), 1000.0F);
         GameRegistry.addRecipe(new ItemStack(Blocks.LADDER, 4, 0), new Object[]{"W W","WWW","W W", 'W', new ItemStack(DABItems.weed, 1, 0)});
         GameRegistry.addShapelessRecipe(new ItemStack(Blocks.TNT, 64, 0), new Object[]{DABItems.pussy, DABItems.Derek});
-        GameRegistry.addSmelting(Items.LEATHER, new ItemStack(Items.ROTTEN_FLESH, 1, 0), 10.0F);
+        GameRegistry.addSmelting(Items.ROTTEN_FLESH, new ItemStack(Items.LEATHER, 1, 0), 10.0F);
         GameRegistry.addShapelessRecipe(new ItemStack(Items.DYE, 9, 15), new Object[]{Blocks.field_189880_di});
     }
 }
