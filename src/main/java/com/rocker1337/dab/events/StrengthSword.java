c9461f814754f50d89e10ff98ff9a533d85e41e0
ninjawarrior1337 <t.rothled@gmail.com>
2016-07-01 18:19:12 -0700

    Things

--- src/main/java/com/rocker1337/dab/events/StrengthSword.java
+++ src/main/java/com/rocker1337/dab/events/StrengthSword.java
@@ -9,14 +9,18 @@ import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-public class StrengthSword {
+public class StrengthSword
+{
 
     @SubscribeEvent(priority = EventPriority.NORMAL, receiveCanceled = true)
-    public void onEvent(LivingEvent.LivingUpdateEvent event) {
-        if (event.getEntity() instanceof EntityPlayer) {
+    public void onEvent(LivingEvent.LivingUpdateEvent event)
+    {
+        if (event.getEntity() instanceof EntityPlayer)
+        {
             EntityPlayer entity = (EntityPlayer) event.getEntity();
             ItemStack heldItem = entity.getHeldItemMainhand();
-            if (heldItem != null && heldItem.getItem() == Items.DIAMOND_SWORD) {
+            if (heldItem != null && heldItem.getItem() == Items.DIAMOND_SWORD)
+            {
                 entity.addPotionEffect(new PotionEffect(Potion.getPotionFromResourceLocation("strength"), 25, 1));
             }
         }
