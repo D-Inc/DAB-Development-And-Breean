f965320a66ceb40d7b916914481982394757c26a
Rocker545 <acidpics@gmail.com>
2016-07-02 15:08:12 -0700

    Fixed Events

--- src/main/java/com/rocker1337/dab/init/items/LessFlight.java
+++ src/main/java/com/rocker1337/dab/init/items/LessFlight.java
@@ -1,6 +1,14 @@
 package com.rocker1337.dab.init.items;
 
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+import java.util.List;
 
 public class LessFlight extends Item
 {
@@ -8,5 +16,10 @@ public class LessFlight extends Item
     {
         setUnlocalizedName(ItemSetNames.DABItems.LESSFLIGHT.getUnlocalizedName());
         setRegistryName(ItemSetNames.DABItems.LESSFLIGHT.getRegistryName());
+    }
+    @SideOnly(Side.CLIENT)
+    public void addInformation(ItemStack Item, List<String> tooltip, boolean advanced){
+        tooltip.add("Lesser Flight");
+
     }
 }
