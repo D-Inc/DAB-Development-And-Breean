a5fa2411c215c2b083dd066a8a4004f90097427e
Rocker545 <acidpics@gmail.com>
2016-07-03 22:26:36 -0700

    Fixed all flying item events! YAY!

---
+++ src/main/java/com/rocker1337/dab/events/SuperEvent.java
@@ -0,0 +1,47 @@
+package com.rocker1337.dab.events;
+
+import com.rocker1337.dab.init.DABItems;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.event.entity.living.LivingEvent;
+import net.minecraftforge.fml.common.eventhandler.EventPriority;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+/**
+ * Created by Rocker545 on 7/3/2016.
+ */
+public class SuperEvent {
+
+    static EntityPlayer player;
+    static boolean hasLessFlight;
+    static boolean hasGreatFlight;
+
+    @SubscribeEvent(priority = EventPriority.NORMAL, receiveCanceled = true)
+    public void onEvent(LivingEvent.LivingUpdateEvent event) {
+        event.getEntity();
+        if (event.getEntity() instanceof EntityPlayer) {
+            player = (EntityPlayer) event.getEntity();
+            ItemStack heldItem = player.getHeldItemMainhand();
+
+            if (heldItem != null && heldItem.getItem() == DABItems.LessFlight) {
+                player.capabilities.allowFlying = true;
+                hasLessFlight = true;
+            } else {
+                hasLessFlight = false;
+            }
+
+
+            if (player.inventory.hasItemStack(new ItemStack(DABItems.GreatFlight))) {
+                player.capabilities.allowFlying = true;
+                hasGreatFlight = true;
+            } else {
+                hasGreatFlight = false;
+            }
+            if (hasGreatFlight == false & hasLessFlight == false & player.capabilities.isCreativeMode == false) {
+                player.capabilities.isFlying = false;
+                player.capabilities.allowFlying = false;
+            }
+        }
+    }
+}
