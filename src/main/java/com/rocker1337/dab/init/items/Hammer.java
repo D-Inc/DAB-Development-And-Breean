b8185d2708e8bd15ed1a564797cb232efd9030d7
Rocker545 <acidpics@gmail.com>
2016-07-06 16:05:41 -0700

    Added a hammer Item!

---
+++ src/main/java/com/rocker1337/dab/init/items/Hammer.java
@@ -0,0 +1,30 @@
+package com.rocker1337.dab.init.items;
+
+import cofh.api.item.IToolHammer;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.common.Optional;
+
+/**
+ * Created by Rocker545 on 7/6/2016.
+ */
+@Optional.InterfaceList({
+        @Optional.Interface(iface = "cofh.api.item.IToolHammer", modid = "CoFHLib")})
+public class Hammer extends Item implements IToolHammer {
+
+    public Hammer() {
+        setMaxStackSize(1);
+    }
+
+    @Override
+    @Optional.Method(modid = "CoFHLib")
+    public boolean isUsable(ItemStack item, EntityLivingBase user, int x, int y, int z){
+        return true;
+    }
+
+    @Override
+    @Optional.Method(modid = "CoFHLib")
+    public void toolUsed(ItemStack item, EntityLivingBase user, int x, int y ,int z){
+    }
+}
