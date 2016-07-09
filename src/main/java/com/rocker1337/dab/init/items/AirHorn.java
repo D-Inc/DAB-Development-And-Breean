73fb990a6cc5fab45fa3b7f0b4c9894ed34e1006
ninjawarrior1337 <t.rothled@gmail.com>
2016-07-09 11:42:42 -0700

    Updated gradle file

--- src/main/java/com/rocker1337/dab/init/items/AirHorn.java
+++ src/main/java/com/rocker1337/dab/init/items/AirHorn.java
@@ -1,21 +1,14 @@
 package com.rocker1337.dab.init.items;
 
 import com.rocker1337.dab.init.SoundEvents.RegisterSoundEvents;
-import net.minecraft.entity.item.EntityTNTPrimed;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.SoundEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.potion.Potion;
-import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.world.World;
-import net.minecraftforge.client.event.sound.SoundEvent;
-
-import java.util.Random;
 
 public class AirHorn extends Item
 {
