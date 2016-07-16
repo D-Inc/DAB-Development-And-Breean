52420c7c947ecb6990681ad8f5c5d0f2253d63d8
ninjawarrior1337 <t.rothled@gmail.com>
2016-07-16 12:45:11 -0700

    Added new music disc

--- src/main/java/com/rocker1337/dab/init/items/MLGCanCanDisk.java
+++ src/main/java/com/rocker1337/dab/init/items/MLGCanCanDisk.java
@@ -1,18 +1,10 @@
 package com.rocker1337.dab.init.items;
 
-import com.google.common.collect.Maps;
-import com.rocker1337.dab.init.SoundEvents.RegisterSoundEvents;
-import net.minecraft.client.audio.Sound;
-import net.minecraft.init.SoundEvents;
+import com.rocker1337.dab.Reference;
 import net.minecraft.item.ItemRecord;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundEvent;
 
-import java.util.Map;
-
-/**
- * Created by ninjawarrior1337 on 7/15/16.
- */
 public class MLGCanCanDisk extends ItemRecord
 {
     protected MLGCanCanDisk(String name, SoundEvent soundIn)
@@ -20,14 +12,12 @@ public class MLGCanCanDisk extends ItemRecord
         super(name, soundIn);
         setUnlocalizedName(SetItemNames.DABItems.CANCANRECORD.getUnlocalizedName());
         setRegistryName(SetItemNames.DABItems.CANCANRECORD.getRegistryName());
-        this.setMaxStackSize(1);
-
     }
 
     @Override
     public ResourceLocation getRecordResource(String name)
     {
-        return new ResourceLocation("dab:"+name);
+        return new ResourceLocation(Reference.MODID + ":" + name);
     }
 }
 
