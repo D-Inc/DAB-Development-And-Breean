142f028828545f23bbfb11a5973bf5b1a9a65d7d
ninjawarrior1337 <t.rothled@gmail.com>
2016-07-31 14:03:09 -0700

    Added Infinity Armor texture

--- src/main/java/com/rocker1337/dab/init/items/InfinityArmor.java
+++ src/main/java/com/rocker1337/dab/init/items/InfinityArmor.java
@@ -7,9 +7,8 @@ import net.minecraftforge.event.entity.living.LivingHurtEvent;
 
 public class InfinityArmor extends ItemArmor
 {
-    LivingHurtEvent e;
-
-    public InfinityArmor(String unlocalizedname, String registryname, ArmorMaterial materialIn, int renderIndexIn, EntityEquipmentSlot equipmentSlotIn) {
+    public InfinityArmor(String unlocalizedname, String registryname, ArmorMaterial materialIn, int renderIndexIn, EntityEquipmentSlot equipmentSlotIn)
+    {
         super(materialIn, renderIndexIn, equipmentSlotIn);
         this.setUnlocalizedName(unlocalizedname);
         this.setRegistryName(registryname);
