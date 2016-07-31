63bf00bec01e7cb8ddf51391a1c298f4556016a1
ninjawarrior1337 <t.rothled@gmail.com>
2016-07-31 16:45:31 -0700

    Make xp picked up double when you hold thorium sword

---
+++ src/main/java/com/rocker1337/dab/init/items/ThoriumCore.java
@@ -0,0 +1,12 @@
+package com.rocker1337.dab.init.items;
+
+import net.minecraft.item.Item;
+
+public class ThoriumCore extends Item
+{
+    public ThoriumCore()
+    {
+        setUnlocalizedName("thorium_core");
+        setRegistryName("thorium_core");
+    }
+}
