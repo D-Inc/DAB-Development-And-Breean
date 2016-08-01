09ee8e7b11cdc220584fbc77d9e84db28ed883ec
ninjawarrior1337 <t.rothled@gmail.com>
2016-07-31 19:01:39 -0700

    Added config for xp multiplier

--- src/main/java/com/rocker1337/dab/ConfigHandler.java
+++ src/main/java/com/rocker1337/dab/ConfigHandler.java
@@ -13,6 +13,7 @@ public class ConfigHandler {
 
     //Settings
     public static boolean chestplateMagnet;
+    public static int swordxpmultiplier;
     public static boolean test;
 
     public static void init(File file)
@@ -23,6 +24,7 @@ public class ConfigHandler {
 
     private static void syncConfig() {
         chestplateMagnet = config.getBoolean("Thorium Chestplate Item Magnet", Configuration.CATEGORY_GENERAL, true, "True to enable magnet");
+        swordxpmultiplier = config.getInt("Thorium Sword XP Multiplier", Configuration.CATEGORY_GENERAL, 2, 1, 2147483647, "This is the XP multiplyer when a player is holding the Thorium Sowrd");
         config.save();
     }
 }
--- src/main/java/com/rocker1337/dab/events/SuperEvent.java
+++ src/main/java/com/rocker1337/dab/events/SuperEvent.java
@@ -1,35 +1,19 @@
 package com.rocker1337.dab.events;
 
-import baubles.common.container.InventoryBaubles;
-import baubles.common.lib.PlayerHandler;
-import com.rocker1337.dab.init.SoundEvents.RegisterSoundEvents;
-import com.rocker1337.dab.init.blocks.DABBlocks;
 import com.rocker1337.dab.init.items.DABItems;
-import com.rocker1337.dab.init.items.GreatFlight;
-import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.SoundEvents;
 import net.minecraft.item.ItemStack;
-import net.minecraft.network.play.client.CPacketPlayerTryUseItem;
-import net.minecraft.network.play.client.CPacketPlayerTryUseItemOnBlock;
 import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionEffect;
-import net.minecraft.util.EnumHand;
-import net.minecraft.util.SoundCategory;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import net.minecraft.world.storage.WorldInfo;
-import net.minecraftforge.client.event.sound.PlaySoundEvent;
-import net.minecraftforge.client.event.sound.SoundEvent;
 import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.event.entity.player.PlayerPickupXpEvent;
-import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 import static com.rocker1337.dab.init.items.GreatFlight.getGreatRing;
-import static com.rocker1337.dab.init.items.GreatFlight.isGreatRing;
+import static com.rocker1337.dab.ConfigHandler.swordxpmultiplier;
 
 /**
  * Created by Rocker545 on 7/3/2016.
@@ -52,6 +36,7 @@ public class SuperEvent {
             player = (EntityPlayer) event.getEntity();
             isCreativeMode = player.capabilities.isCreativeMode;
             ItemStack heldItem = player.getHeldItemMainhand();
+
             // LessFlightEvent
             if (heldItem != null && heldItem.getItem() == DABItems.LessFlight) {
                 player.capabilities.allowFlying = true;
@@ -116,7 +101,7 @@ public class SuperEvent {
         ItemStack heldItem = player.getHeldItemMainhand();
         if(heldItem != null && heldItem.getItem() == DABItems.thoriumsword);
         {
-            xpEvent.getOrb().xpValue *= 2;
+            xpEvent.getOrb().xpValue *=swordxpmultiplier;
             System.out.println("XP Doubbled");
         }
     }
