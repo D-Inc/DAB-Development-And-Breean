db6a0caf6c432fd3695e004ed904b751ddd6dbb6
Rocker545 <acidpics@gmail.com>
2016-08-03 19:18:04 -0700

    meow

--- src/main/java/com/rocker1337/dab/packets/KeyPressPKT.java
+++ src/main/java/com/rocker1337/dab/packets/KeyPressPKT.java
@@ -48,7 +48,7 @@ public class KeyPressPKT implements IMessage{
                         if(heldItem.getTagCompound().getFloat("Speed") != 2147483640.0F)
                         {
                             float speedy = heldItem.getTagCompound().getFloat("Speed");
-                            heldItem.getTagCompound().setFloat("Speed", speedy+5.0F);
+                            heldItem.getTagCompound().setFloat("Speed", speedy + 5.0F);
                         }
                        /* else if(heldItem.getTagCompound().getFloat("Speed") == 20F)
                         {
@@ -65,7 +65,7 @@ public class KeyPressPKT implements IMessage{
                         if(heldItem.getTagCompound().getFloat("Speed") != 15.0F)
                         {
                             float sonic = heldItem.getTagCompound().getFloat("Speed");
-                            heldItem.getTagCompound().setFloat("Speed", sonic-5.0F);
+                            heldItem.getTagCompound().setFloat("Speed", sonic - 5.0F);
                         }
                        /* else if(heldItem.getTagCompound().getFloat("Speed") == 25F)
                         {
