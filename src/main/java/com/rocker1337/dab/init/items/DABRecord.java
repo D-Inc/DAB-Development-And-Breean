b02a37334ebe7550035f85ead3dbd6ba9b5bc7fe
ninjawarrior1337 <t.rothled@gmail.com>
2016-08-04 19:40:45 -0700

    Music disc changes

--- src/main/java/com/rocker1337/dab/init/items/DABRecord.java
+++ src/main/java/com/rocker1337/dab/init/items/DABRecord.java
@@ -2,15 +2,22 @@ package com.rocker1337.dab.init.items;
 
 import com.rocker1337.dab.Reference;
 import net.minecraft.item.ItemRecord;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundEvent;
 
 public class DABRecord extends ItemRecord
 {
-    protected DABRecord(String recordName, SoundEvent soundEvent)
+    public DABRecord(String recordName, SoundEvent soundEvent)
     {
         super(Reference.RESOURCE_PREFIX + recordName, soundEvent);
         setRegistryName("record_" + recordName);
         setUnlocalizedName("record");
+    }
+
+    @Override
+    public ResourceLocation getRecordResource(String name)
+    {
+        return new ResourceLocation(Reference.MODID, name);
     }
 }
 
