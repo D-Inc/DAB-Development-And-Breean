b9f32b545062d008c5e31828e1d838d2e1aa7887
Rocker545 <acidpics@gmail.com>
2016-08-04 19:01:49 -0700

    updated config entry descriptions

--- src/main/java/com/rocker1337/dab/ConfigHandler.java
+++ src/main/java/com/rocker1337/dab/ConfigHandler.java
@@ -29,7 +29,7 @@ public class ConfigHandler {
     public static void syncConfig()
     {
         chestplateMagnet = config.getBoolean("Thorium Chestplate Item Magnet", CATEGORY_GENERAL, true, "True to enable magnet");
-        swordxpmultiplier = config.getInt("Thorium Sword XP Multiplier", CATEGORY_GENERAL, 2, 1, 2147483647, "This is the XP multiplier when a player is holding the Thorium Sword");
+        swordxpmultiplier = config.getInt("Thorium Sword & Shickaxe XP Multiplier", CATEGORY_GENERAL, 2, 1, 2147483647, "This is the XP multiplier when a player is holding the Thorium Sword");
         multitoolaoemining = config.getBoolean("Shickaxe Ore AOE Mining", CATEGORY_GENERAL, true, "When you click all ores around you get mined and put into inventory");
         if(config.hasChanged())
         {
