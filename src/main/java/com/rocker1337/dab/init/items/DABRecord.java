40b73615beb56aed159e85b4dbadc95ba0333986
ninjawarrior1337 <t.rothled@gmail.com>
2016-08-21 16:41:07 -0700

    Merge branch 'master' of /home/ninjawarrior1337/IdeaProjects/DAB-Development-And-Breean- with conflicts.

--- src/main/java/com/rocker1337/dab/init/items/DABRecord.java
+++ src/main/java/com/rocker1337/dab/init/items/DABRecord.java
@@ -9,7 +9,7 @@ public class DABRecord extends ItemRecord
 {
     public DABRecord(String recordName, SoundEvent soundEvent)
     {
-        super(Reference.RESOURCE_PREFIX + recordName, soundEvent);
+        super(recordName, soundEvent);
         setRegistryName("record_" + recordName);
         setUnlocalizedName("record");
     }
@@ -17,7 +17,7 @@ public class DABRecord extends ItemRecord
     @Override
     public ResourceLocation getRecordResource(String name)
     {
-        return new ResourceLocation(Reference.MODID, name);
+        return new ResourceLocation(Reference.RESOURCE_PREFIX+name);
     }
 }
 
