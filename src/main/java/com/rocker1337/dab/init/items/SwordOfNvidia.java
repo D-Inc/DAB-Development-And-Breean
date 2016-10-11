6a24bf961928f5e6b5d4ada2b4d1fdd14774f9f5
ninjawarrior1337 <t.rothled@gmail.com>
2016-10-10 22:14:18 -0700

    fixed build.gradle

--- src/main/java/com/rocker1337/dab/init/items/SwordOfNvidia.java
+++ src/main/java/com/rocker1337/dab/init/items/SwordOfNvidia.java
@@ -1,18 +1,18 @@
 package com.rocker1337.dab.init.items;
 
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.ItemSword;
 
 /**
  * Created by ninjawarrior1337 on 9/3/16.
  */
-public class SwordOfNvidia extends ItemSword
+public class SwordOfNvidia extends Item
 {
-
     public SwordOfNvidia(ToolMaterial material)
     {
-        super(material);
+        super(/*material*/);
         setUnlocalizedName("swordofnvivia");
         setRegistryName("swordofnividia");
     }
@@ -21,7 +21,18 @@ public class SwordOfNvidia extends ItemSword
     public boolean hitEntity(ItemStack stack, EntityLivingBase target, EntityLivingBase attacker)
     {
         stack.damageItem(1, attacker);
-        target.setHealth(target.getHealth()+1);
+        if (attacker.isSneaking())
+        {
+            target.setHealth(target.getHealth()+4);
+        }
+        else
+        {
+            target.setHealth(target.getHealth()-1);
+            ((EntityPlayer) target).inventory.armorItemInSlot(0).damageItem(((EntityPlayer) target).inventory.armorItemInSlot(0).getItemDamage()/2, target);
+            ((EntityPlayer) target).inventory.armorItemInSlot(1).damageItem(((EntityPlayer) target).inventory.armorItemInSlot(1).getItemDamage()/2, target);
+            ((EntityPlayer) target).inventory.armorItemInSlot(2).damageItem(((EntityPlayer) target).inventory.armorItemInSlot(2).getItemDamage()/2, target);
+            ((EntityPlayer) target).inventory.armorItemInSlot(3).damageItem(((EntityPlayer) target).inventory.armorItemInSlot(3).getItemDamage()/2, target);
+        }
         return true;
     }
 }
