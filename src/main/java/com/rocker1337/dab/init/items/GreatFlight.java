0bdf6803dd29ead4039e2261ad94a25dc9cf0d60
Rocker545 <acidpics@gmail.com>
2016-11-06 12:01:13 -0800

    fix bauble/grat tring

--- src/main/java/com/rocker1337/dab/init/items/GreatFlight.java
+++ src/main/java/com/rocker1337/dab/init/items/GreatFlight.java
@@ -1,9 +1,9 @@
 package com.rocker1337.dab.init.items;
 
 import baubles.api.BaubleType;
-import baubles.common.container.InventoryBaubles;
+import baubles.api.BaublesApi;
+import baubles.api.cap.IBaublesItemHandler;
 import baubles.common.items.ItemRing;
-import baubles.common.lib.PlayerHandler;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
@@ -13,7 +13,7 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 
 import java.util.List;
 
-public class GreatFlight extends ItemRing {
+public class GreatFlight extends ItemRing{
     public GreatFlight() {
         setUnlocalizedName(SetItemNames.DABItems.GREATFLIGHT.getUnlocalizedName());
         setRegistryName(SetItemNames.DABItems.GREATFLIGHT.getRegistryName());
@@ -33,7 +33,8 @@ public class GreatFlight extends ItemRing {
     }
 
     public static ItemStack getGreatRing(EntityPlayer player) {
-        InventoryBaubles baubles = PlayerHandler.getPlayerBaubles(player);
+        IBaublesItemHandler baubles = BaublesApi.getBaublesHandler(player);
+
         ItemStack stack1 = baubles.getStackInSlot(1);
         ItemStack stack2 = baubles.getStackInSlot(2);
         return isGreatRing(stack1) ? stack1 : isGreatRing(stack2) ? stack2 : null;
