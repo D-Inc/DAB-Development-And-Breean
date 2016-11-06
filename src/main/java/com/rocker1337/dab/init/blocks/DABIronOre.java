d9063d9feb32c5322edd77caa083f04856d2704d
ninjawarrior1337 <t.rothled@gmail.com>
2016-11-06 10:34:04 -0800

    Lots Of Stuff Just happened

---
+++ src/main/java/com/rocker1337/dab/init/blocks/DABIronOre.java
@@ -0,0 +1,7 @@
+package com.rocker1337.dab.init.blocks;
+
+/**
+ * Created by ninjawarrior1337 on 10/27/16.
+ */
+public class DABIronOre {
+}
---
+++ src/main/java/com/rocker1337/dab/init/items/DABIronIngot.java
@@ -0,0 +1,7 @@
+package com.rocker1337.dab.init.items;
+
+/**
+ * Created by ninjawarrior1337 on 10/27/16.
+ */
+public class DABIronIngot {
+}
