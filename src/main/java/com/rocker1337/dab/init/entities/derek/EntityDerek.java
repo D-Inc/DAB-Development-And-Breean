922855f620c2c9859b80cce081560fb21b8b0b5b
ninjawarrior1337 <t.rothled@gmail.com>
2016-11-14 17:11:34 -0800

    Fixed the insane derek spawn rate and make him hit harder

--- src/main/java/com/rocker1337/dab/init/entities/derek/EntityDerek.java
+++ src/main/java/com/rocker1337/dab/init/entities/derek/EntityDerek.java
@@ -5,6 +5,8 @@ import net.minecraft.entity.EntityCreature;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.EntityAIAttackMelee;
 import net.minecraft.entity.ai.EntityAINearestAttackableTarget;
+import net.minecraft.entity.ai.EntityAIWander;
+import net.minecraft.entity.monster.EntityMob;
 import net.minecraft.entity.passive.EntitySheep;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
@@ -13,7 +15,7 @@ import net.minecraft.world.World;
 /**
  * Created by troth on 9/11/2016.
  */
-public class EntityDerek extends EntityCreature
+public class EntityDerek extends EntityMob
 {
     public EntityDerek(World worldIn) {
         super(worldIn);
@@ -24,12 +26,14 @@ public class EntityDerek extends EntityCreature
         super.applyEntityAttributes();
         getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.2D);
         getEntityAttribute(SharedMonsterAttributes.FOLLOW_RANGE).setBaseValue(40.0D);
+        getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).setBaseValue(4.0D);
     }
 
     @Override
     protected void initEntityAI()
     {
         tasks.addTask(1, new EntityAIAttackMelee(this, 10D, true));
+        tasks.addTask(1, new EntityAIWander(this, 1D));
         this.targetTasks.addTask(1, new EntityAINearestAttackableTarget(this, EntityPlayer.class, true));
         this.targetTasks.addTask(1, new EntityAINearestAttackableTarget(this, EntitySheep.class, false));
     }
