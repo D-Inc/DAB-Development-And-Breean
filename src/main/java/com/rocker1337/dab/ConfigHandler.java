71e25d212879c874d9ad0ef3c70660b31a342b32
ninjawarrior1337 <t.rothled@gmail.com>
2016-11-19 10:03:59 -0800

    Gave dab iron ore a texture and made the generation stuff different

--- src/main/java/com/rocker1337/dab/ConfigHandler.java
+++ src/main/java/com/rocker1337/dab/ConfigHandler.java
@@ -18,6 +18,11 @@ public class ConfigHandler {
     public static boolean multitoolaoemining;
     public static float thoriumleggingstepassist;
     public static boolean noTallGrass;
+    public static boolean noFlowers;
+    public static boolean noTrees;
+    public static int treeSpawnPercentage;
+    public static int flowerSpawnPercentage;
+    public static int grassSpawnPercentage;
 
     public static void init(File file)
     {
@@ -34,7 +39,12 @@ public class ConfigHandler {
         swordxpmultiplier = config.getInt("Thorium Sword & Shickaxe XP Multiplier", CATEGORY_GENERAL, 2, 1, 2147483647, "This is the XP multiplier when a player is holding the Thorium Sword");
         multitoolaoemining = config.getBoolean("Shickaxe Ore AOE Mining", CATEGORY_GENERAL, true, "When you click all ores around you get mined and put into inventory");
         thoriumleggingstepassist = config.getFloat("Thorium Legging Step Assist Height", CATEGORY_GENERAL, 1.0F, 0F, 2147483647F, "How many blocks you can go up without jumping");
-        noTallGrass = config.getBoolean("Generate Tall Grass", CATEGORY_GENERAL, true, "This will turn off the generation of tall and doubble tall grass (You can still use bonemeal)");
+//        noTallGrass = config.getBoolean("Generate Tall Grass", CATEGORY_GENERAL, true, "This will turn off the generation of tall and doubble tall grass (You can still use bonemeal)");
+//        noFlowers = config.getBoolean("Generate Flowers", CATEGORY_GENERAL, true, "This will turn off the generation of minecraft flowers in the world (You can still use bonemean)");
+//        noTrees = config.getBoolean("Generate Trees (IDK why anyone would need this)", CATEGORY_GENERAL, true, null);
+        treeSpawnPercentage = config.getInt("Generate Tree Percentage", CATEGORY_GENERAL, 100, 0, 100, null);
+        flowerSpawnPercentage = config.getInt("Generate Flower Percentage", CATEGORY_GENERAL, 100, 0, 100, null);
+        grassSpawnPercentage = config.getInt("Generate Grass Percentage", CATEGORY_GENERAL, 100, 0, 100, null);
         if(config.hasChanged())
         {
             config.save();
--- src/main/java/com/rocker1337/dab/events/TerrainEventHandler.java
+++ src/main/java/com/rocker1337/dab/events/TerrainEventHandler.java
@@ -5,16 +5,54 @@ import net.minecraftforge.event.terraingen.DecorateBiomeEvent;
 import net.minecraftforge.fml.common.eventhandler.Event;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
+import java.util.Random;
+
 /**
  * Created by drkpa on 11/15/2016.
  */
-public class TerrainEventHandler {
+public class TerrainEventHandler
+{
+    public static boolean getRandomTreeBoolean()
+    {
+        return Math.random() < ConfigHandler.treeSpawnPercentage/100;
+    }
+
+    public static boolean getRandomFlowerBoolean()
+    {
+        return Math.random() < ConfigHandler.flowerSpawnPercentage/100;
+    }
+
+    public static boolean getRandomGrassBoolean()
+    {
+        return Math.random() < ConfigHandler.grassSpawnPercentage/100;
+    }
 
     @SubscribeEvent(receiveCanceled = true)
     public void onDecorateEvent(DecorateBiomeEvent.Decorate event)
     {
 
-        if(event.getType() == DecorateBiomeEvent.Decorate.EventType.GRASS && !ConfigHandler.noTallGrass)
+//        if(event.getType() == DecorateBiomeEvent.Decorate.EventType.GRASS && !ConfigHandler.noTallGrass)
+//        {
+//            event.setResult(Event.Result.DENY);
+//        }
+//        if(event.getType() == DecorateBiomeEvent.Decorate.EventType.FLOWERS && !ConfigHandler.noFlowers)
+//        {
+//            event.setResult(Event.Result.DENY);
+//        }
+//        if (event.getType() == DecorateBiomeEvent.Decorate.EventType.TREE && !ConfigHandler.noTrees)
+//        {
+//            event.setResult(Event.Result.DENY);
+//        }
+
+        if(event.getType() == DecorateBiomeEvent.Decorate.EventType.GRASS && !getRandomGrassBoolean())
+        {
+            event.setResult(Event.Result.DENY);
+        }
+        if(event.getType() == DecorateBiomeEvent.Decorate.EventType.FLOWERS && !getRandomFlowerBoolean())
+        {
+            event.setResult(Event.Result.DENY);
+        }
+        if (event.getType() == DecorateBiomeEvent.Decorate.EventType.TREE && !getRandomTreeBoolean())
         {
             event.setResult(Event.Result.DENY);
         }
