ccafe41898e72f53477104d96e9e0be2a6d85062
Rocker545 <acidpics@gmail.com>
2016-11-18 17:22:05 -0800

    derek > tyler = seek

--- src/main/java/com/rocker1337/dab/events/TerrainEventHandler.java
+++ src/main/java/com/rocker1337/dab/events/TerrainEventHandler.java
@@ -1,5 +1,6 @@
 package com.rocker1337.dab.events;
 
+import com.rocker1337.dab.ConfigHandler;
 import net.minecraftforge.event.terraingen.DecorateBiomeEvent;
 import net.minecraftforge.fml.common.eventhandler.Event;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -12,7 +13,8 @@ public class TerrainEventHandler {
     @SubscribeEvent(receiveCanceled = true)
     public void onDecorateEvent(DecorateBiomeEvent.Decorate event)
     {
-        if(event.getType() == DecorateBiomeEvent.Decorate.EventType.GRASS)
+
+        if(event.getType() == DecorateBiomeEvent.Decorate.EventType.GRASS && !ConfigHandler.noTallGrass)
         {
             event.setResult(Event.Result.DENY);
         }
